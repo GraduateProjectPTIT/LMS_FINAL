f7b9d7c698121bc19bf8cc285a2b9a773ceb24b4
BinhMinhPTIT <binhminhtt03@gmail.com>
2025-08-10 21:37:58 +0700

    fix: Fix bug on Add review, question, Add checking category, Refactor service and controller

---
+++ server/interfaces/course.interface.ts
@@ -0,0 +1,25 @@
+// Course Controller Interfaces
+
+export interface IAddQuestionData {
+  question: string;
+  courseId: string;
+  contentId: string;
+}
+
+export interface IAddAnswerData {
+  answer: string;
+  courseId: string;
+  contentId: string;
+  questionId: string;
+}
+
+export interface IAddReviewData {
+  review: string;
+  rating: number;
+}
+
+export interface IAddReviewReplyData {
+  comment: string;
+  courseId: string;
+  reviewId: string;
+}
