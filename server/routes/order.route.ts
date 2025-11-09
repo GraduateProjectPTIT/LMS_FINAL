18a786a48b9eced83e77b118539dc99fba7c8d30
BinhMinhPTIT <binhminhtt03@gmail.com>
2025-11-09 21:03:23 +0700

    feat: Add user get order detail

--- server/routes/order.route.ts
+++ server/routes/order.route.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
-import { createOrder, getAdminOrders, getUserOrders, getPaidOrders, getAdminOrderDetail, getTutorOrders, getTutorOrderDetail } from "../controllers/order.controller";
+import { createOrder, getAdminOrders, getUserOrders, getPaidOrders, getAdminOrderDetail, getTutorOrders, getTutorOrderDetail, getUserOrderDetail } from "../controllers/order.controller";
 
 const orderRouter = express.Router();
 
 orderRouter.post("/order/create_order", isAuthenticated, createOrder);
 orderRouter.get("/order/get_all_orders", isAuthenticated, authorizeRoles("admin"), getAdminOrders);
 orderRouter.get("/order/get_user_orders", isAuthenticated, getUserOrders);
+orderRouter.get("/order/get_user_order_detail/:id", isAuthenticated, getUserOrderDetail);
 orderRouter.get("/order/get_paid_orders", isAuthenticated, authorizeRoles("admin"), getPaidOrders);
 orderRouter.get("/order/get_order_detail/:id", isAuthenticated, authorizeRoles("admin"), getAdminOrderDetail);
 orderRouter.get("/order/tutor/get_orders", isAuthenticated, authorizeRoles("tutor"), getTutorOrders);
